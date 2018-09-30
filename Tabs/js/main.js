b2998e63dd4f2ea402bb08f0b6fb65e2c6e0b88d
zhangyongwang <349368992@qq.com>
2018-09-30 09:19:49 +0800

    利用原型进行优化组件

--- Tabs/js/main.js
+++ Tabs/js/main.js
@@ -1,9 +1,36 @@
+// function Tabs(selector) {
+//     this.elements = $(selector)
+//     this.elements.each(function (index, elements) {
+//         $(elements).children('.tabs-bar').children('li').eq(0).addClass('active')
+//         $(elements).children('.tabs-content').children('li').eq(0).addClass('active')
+//     })
+//     this.elements.on('click', '.tabs-bar > li', function (e) {
+//         var $li = $(e.currentTarget)
+//         $li.addClass('active').siblings().removeClass('active')
+//         var index = $li.index()
+//         var $content = $li.closest('.tabs').find('.tabs-content >li').eq(index)
+//         $content.addClass('active').siblings().removeClass('active')
+//     })
+// }
+
+// var tabs = new Tabs('.xxx')
+
+//利用原型优化
+
 function Tabs(selector) {
     this.elements = $(selector)
+    this.init()
+    this.bindEvents()
+}
+
+Tabs.prototype.init = function () {
     this.elements.each(function (index, elements) {
         $(elements).children('.tabs-bar').children('li').eq(0).addClass('active')
         $(elements).children('.tabs-content').children('li').eq(0).addClass('active')
     })
+}
+
+Tabs.prototype.bindEvents = function () {
     this.elements.on('click', '.tabs-bar > li', function (e) {
         var $li = $(e.currentTarget)
         $li.addClass('active').siblings().removeClass('active')
